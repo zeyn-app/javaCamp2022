b7e4360b487607b1d515b283c89ed68486ebec63
Zeynep Özdemir <zeynepozz2299@gmail.com>
2022-10-25 10:10:50 +0300

    Üçüncü haftanın ilk ödevi silindi.

---
+++ Ödev3/Ödev3.1/src/MilitaryCreditManager.java
@@ -0,0 +1,17 @@
+public class MilitaryCreditManager extends BaseCreditManager implements ICreditManager{
+
+	@Override
+	public void Calculate() {
+		System.out.println("Asker kredisi hesaplandı");
+		
+	}
+/*
+ * Bu fonksiyonu, bu sınıfta yazmaya gerek yok artık.
+ */
+	/*@Override
+	public void Save() {
+		System.out.println("Yeni kayıt başarılı.");
+		
+	}*/
+
+}
---
+++ Ödev3/Ödev3.1/src/Person.java
@@ -0,0 +1,27 @@
+
+public class Person extends Cusstomer{
+	public String firstName;
+	public String lastName;
+	public String NationalIdentity;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNationalIdentity() {
+		return NationalIdentity;
+	}
+	public void setNationalIdentity(String nationalIdentity) {
+		NationalIdentity = nationalIdentity;
+	}
+
+
+}
