6f7cf0f7a78314936e70bc431737bed36b6b8541
Zeynep Özdemir <zeynepozz2299@gmail.com>
2022-10-25 10:08:47 +0300

    Üçüncü haftanın ilk ödevi silindi.

--- Ödev3/Ödev3.1/src/BaseCreditManager.java
+++
@@ -1,7 +0,0 @@
-public abstract class BaseCreditManager implements ICreditManager {
-
-	public abstract void Calculate();
-	public void Save() {
-		System.out.println("Kaydedildi.");
-	}
-}
--- Ödev3/Ödev3.1/src/TeacherCreditManager.java
+++
@@ -1,9 +0,0 @@
-
-public class TeacherCreditManager extends BaseCreditManager implements ICreditManager {
-
-	@Override
-	public void Calculate() {
-		System.out.println("Öğretmen kredisi hesaplandı");
-		
-	}
-}
