ae85a4dac717bfde0bc369872099d5425c94931f
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-05-22 04:00:42 +0300

    The required methods added and their implementations done

--- Up School/Up School Weekly/src/hardSkills_2023_05_21/LibraryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package hardSkills_2023_05_21;
-
-public class LibraryNotFoundException extends Exception {
-
-	public LibraryNotFoundException(String message) {
-		System.out.println(message);
-	}
-}
--- Up School/Up School Weekly/src/hardSkills_2023_05_21/LimitOfBookExceededException.java
+++
@@ -1,8 +0,0 @@
-package hardSkills_2023_05_21;
-
-public class LimitOfBookExceededException extends Exception {
-
-	public LimitOfBookExceededException(String message) {
-		super(message);
-	}
-}
